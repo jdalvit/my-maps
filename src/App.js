c0e76d87ae6bac5282c975db6ee729ab790970f2
j <juandalvit@gmail.com>
2022-01-30 18:57:39 +0100

    Added map component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from "react";
 import "./App.css";
+import { Map } from "./components/Map";
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <Map
+        defaultCenter={{ lat: -34.397, lng: 150.644 }}
+        // TODO: Change marker array to redux state once feature is merged
+        markerArray={[
+          { lat: -34.397, lng: 150.644 },
+          { lat: -35.397, lng: 151.644 },
+          { lat: -33.397, lng: 150.644 },
+        ]}
+        // TODO: Move api URL to .ENV file
+        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyD5pVs8CyH-AMdlzdxNSznUCQzTpFf90wY&v=3.exp&libraries=places"
+      />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Map.jsx
@@ -0,0 +1,30 @@
+import { compose, withProps } from "recompose";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+} from "react-google-maps";
+import React from "react";
+
+const mapElementStyles = { height: `100%`, width: "100%" };
+
+export const Map = compose(
+  withProps({
+    loadingElement: <div style={mapElementStyles} />,
+    containerElement: <div style={mapElementStyles} />,
+    mapElement: <div style={mapElementStyles} />,
+  }),
+  withScriptjs,
+  withGoogleMap
+)(({ markerArray, withDefaultUi, defaultCenter }) => (
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={defaultCenter}
+    options={{ disableDefaultUI: withDefaultUi }}
+  >
+    {markerArray.map((marker) => (
+      <Marker position={marker} />
+    ))}
+  </GoogleMap>
+));
